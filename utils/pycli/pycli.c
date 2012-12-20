0973826d1c89613ba96d0c19fda66eac5fa086de
shenoubang <shenoubang@gmail.com>
2012-12-20 10:24:34 +0800

    Add python to windows for command line
    Add python to windows for command line
    
    Signed-off-by: shenoubang <shenoubang@gmail.com>
    Signed-off-by: David Yu <keweihk@gmail.com>
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- utils/pycli/pycli.c
+++ utils/pycli/pycli.c
@@ -1,9 +1,24 @@
 #include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include "skyeye_types.h"
 #include "skyeye_module.h"
 #include "skyeye_cli.h"
+
+#ifdef __MINGW32__
+int setenv(const char *name, const char *value, int overwrite)
+{
+    int result = 0;
+    if (overwrite || !getenv(name)) {
+        size_t length = strlen(name) + strlen(value) + 2;
+        char *string = malloc(length);
+        snprintf(string, length, "%s=%s", name, value);
+        result = putenv(string);
+    }
+    return result;
+}
+#endif
 
 int
 cli(const char* prompt)
@@ -16,7 +31,11 @@ cli(const char* prompt)
 	//printf("new_path=%s\n", new_path);
 	PyRun_SimpleString("import sys\n");
 	PyRun_SimpleString(new_path);
+#ifndef __MINGW32__
 	PyRun_SimpleString("from skyeye_cli import *\nSkyEyeCli().cmdloop()\n");
+#else
+	PyRun_SimpleString("from skyeye_cli_windows import *\nSkyEyeCliWindows().cmdloop()\n");
+#endif
 	Py_Finalize();
 	return 0;
 }
---
+++ utils/pycli/skyeye_common_windows_module.py
@@ -0,0 +1,23 @@
+#### SkyEye common module ####
+import os
+from ctypes import *
+
+libcommon_path = "C:/msys/1.0/opt/skyeye/bin/libcommon-0.dll"
+
+libcommon = CDLL(libcommon_path, RTLD_GLOBAL)
+
+#### SkyEye other modules ####
+libdisasm_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libdisasm-0.dll"
+libpmon_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libpmon-0.dll"
+libuart_16550_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libuart_16550-0.dll"
+libcodecov_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libcodecov-0.dll"
+libbus_log_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libbus_log-0.dll"
+libgdbserver_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libgdbserver-0.dll"
+
+# Generate CDLL handlers
+libdisasm = CDLL(libdisasm_path, RTLD_LOCAL)
+libpmon = CDLL(libpmon_path, RTLD_LOCAL)
+libuart_16550 = CDLL(libuart_16550_path, RTLD_LOCAL)
+libcodecov = CDLL(libcodecov_path, RTLD_LOCAL)
+libbus_log = CDLL(libbus_log_path, RTLD_LOCAL)
+libgdbserver = CDLL(libgdbserver_path, RTLD_LOCAL)
