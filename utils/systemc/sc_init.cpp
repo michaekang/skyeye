3a290307e2a8c2c2195bfdabbba6c6af1469c78a
Michael.Kang <blackfin.kang@gmail.com>
2013-05-07 21:55:18 +0800

    Register the systemc bus operation
    Register the systemc bus operation
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- utils/systemc/sc_init.cpp
+++ utils/systemc/sc_init.cpp
@@ -1,5 +1,45 @@
+/* Copyright (C) 
+* 2013 - Michael.Kang blackfin.kang@gmail.com
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+* 
+*/
+/**
+* @file sc_init.cpp
+* @brief the systemc initialization
+* @author Michael.Kang blackfin.kang@gmail.com
+* @version 7849
+* @date 2013-05-07
+*/
+
 #include "lt_top.h"
 #include "sc_init.h"
+#include <skyeye_bus.h>
+
+static int sc_bus_read(short size, generic_address_t addr, uint32_t * value){
+	int ret;
+	ret = default_bus_read(size, addr, value);
+	return ret;
+}
+static int sc_bus_write(short size, generic_address_t addr, uint32_t value){
+	int ret;
+	ret = default_bus_write(size, addr, value);
+	return ret;
+}
+
 void init_systemc_class(){
+	register_bus_operation(sc_bus_read, sc_bus_write);
 	Lt_top("lt_top");
+	return;
 }
