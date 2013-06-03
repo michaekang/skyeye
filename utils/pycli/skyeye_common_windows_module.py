55622e65325d2b5b9dadd3edcccc3b4f710cf029
zyuming <zyumingfit@gmail.com>
2013-06-03 14:12:16 +0800

    Modify the path of modules under windows
    Modify the path of modules under windows
    
    Signed-off-by: zyuming <zyumingfit@gmail.com>
    Signed-off-by: Kewei Yu <keweihk@gmail.com>

--- utils/pycli/skyeye_common_windows_module.py
+++ utils/pycli/skyeye_common_windows_module.py
@@ -2,17 +2,17 @@
 import os
 from ctypes import *
 
-libcommon_path = "C:/msys/1.0/opt/skyeye/bin/libcommon-0.dll"
+libcommon_path = "C:/skyeye/1.0/opt/skyeye/bin/libcommon-0.dll"
 
 libcommon = CDLL(libcommon_path, RTLD_GLOBAL)
 
 #### SkyEye other modules ####
-libdisasm_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libdisasm-0.dll"
-libpmon_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libpmon-0.dll"
-libuart_16550_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libuart_16550-0.dll"
-libcodecov_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libcodecov-0.dll"
-libbus_log_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libbus_log-0.dll"
-libgdbserver_path = "C:/msys/1.0/opt/skyeye/lib/skyeye/libgdbserver-0.dll"
+libdisasm_path = "C:/skyeye/1.0/opt/skyeye/lib/skyeye/libdisasm-0.dll"
+libpmon_path = "C:/skyeye/1.0/opt/skyeye/lib/skyeye/libpmon-0.dll"
+libuart_16550_path = "C:/skyeye/1.0/opt/skyeye/lib/skyeye/libuart_16550-0.dll"
+libcodecov_path = "C:/skyeye/1.0/opt/skyeye/lib/skyeye/libcodecov-0.dll"
+libbus_log_path = "C:/skyeye/1.0/opt/skyeye/lib/skyeye/libbus_log-0.dll"
+libgdbserver_path = "C:/skyeye/1.0/opt/skyeye/lib/skyeye/libgdbserver-0.dll"
 
 # Generate CDLL handlers
 libdisasm = CDLL(libdisasm_path, RTLD_LOCAL)
